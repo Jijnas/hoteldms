ce2c499de26694e4f71fed602e6be8ac788570cc
Jijnas S <jijnas1994@gmail.com>
2020-10-12 22:35:29 +0530

    Add files via upload

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {LEADERS} from '../shared/leaders';
+import {LeaderService} from '../services/leader.service';
+import {Leader} from '../shared/leader';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  leaders: Leader[];
+  constructor(private leaderService: LeaderService) { }
+
+  ngOnInit() {
+    this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders);
+  }
+
+}
